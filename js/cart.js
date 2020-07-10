9ba02e356102e2913157d4249ffd2a1943ab9393
farid95g <f95guluzade@gmail.com>
2020-07-10 19:05:16 +0400

    shopping cart functionality improved

--- js/cart.js
+++ js/cart.js
@@ -15,13 +15,12 @@ let order = 1;
 
 // creating the view of the cart page according to the cart length
 if (cart.length === 0) {
-    table.style.display = "none";
-    let message = document.createElement("h1");
-    message.style.padding = "50px 0";
-    message.innerHTML = `<p style="text-align: center;">There is no item in your cart. <br /><br /> Go <a href="../index.html">ahead</a> and add something...</p>`;
-    products.appendChild(message);
+    emptyCart();
 } else {
     cart.forEach(product => {
+        //showing table element
+        table.classList.remove("d-none");
+
         // creating table rows that holds the product added to cart
         let row = document.createElement("tr");
         let orderCell = document.createElement("th");
@@ -29,20 +28,45 @@ if (cart.length === 0) {
         let nameCell = document.createElement("td");
         let priceCell = document.createElement("td");
         let quantityCell = document.createElement("td");
+        let removeCell = document.createElement("td");
         let img = document.createElement("img");
+        let removeButton = document.createElement("a");
 
         // filling the elements with the products' datas
         orderCell.setAttribute("scope", "row");
+        orderCell.className = "align-middle";
         orderCell.innerText = order;
         order++;
         img.setAttribute("src", "../" + product.img);
         nameCell.innerText = product.name;
+        nameCell.className = "align-middle";
         priceCell.innerText = "$" + product.price;
+        priceCell.className = "align-middle";
         quantityCell.innerText = product.quantity;
+        quantityCell.className = "align-middle";
+        removeButton.setAttribute("href", "#");
+        removeButton.innerHTML = "<i class='fas fa-trash-alt'></i>";
+        removeCell.className = "align-middle";
+        let removeStyle = "font-size: 20px; text-decoration: none; color: #000";
+        removeButton.setAttribute("style", removeStyle);
+
+        // adding click event to remove buttons
+        removeButton.onclick = function(e) {
+            e.preventDefault();
+            cart.splice(cart.indexOf(cart.find(p => p.id === product.id)), 1);
+            localStorage.setItem("cart", JSON.stringify(cart));
+            this.parentElement.parentElement.remove();
+            if (cart.length === 0) {
+                table.style.display = "none";
+                emptyCart();
+            }
+            productQuantity();
+        }
 
         // appending elements to the parent elements
         imgCell.appendChild(img);
-        row.append(orderCell, imgCell, nameCell, priceCell, quantityCell);
+        removeCell.appendChild(removeButton);
+        row.append(orderCell, imgCell, nameCell, priceCell, quantityCell, removeCell);
         table.lastElementChild.appendChild(row);
     });
 }
@@ -62,3 +86,12 @@ productQuantity();
 
 // calling productQuantity function each 1 second for updating the product quantity if the user will clear all localStorage items
 setInterval(productQuantity, 1000);
+
+// function for empty cart message
+function emptyCart() {
+    table.style.display = "none";
+    let message = document.createElement("h1");
+    message.style.padding = "200px 0";
+    message.innerHTML = `<p style="text-align: center;">There is no item in your cart. <br /><br /> Go <a href="../index.html">ahead</a> and add something...</p>`;
+    products.appendChild(message);
+}
